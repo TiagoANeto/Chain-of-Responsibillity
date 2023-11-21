d3e52a94ebfaac4ae07468e7e7e888b5687908cf
anaclaragl <ana_claragl@hotmail.com>
2023-11-21 18:20:37 -0300

    Movimentacao inimigo

---
+++ PDJ_COR/Assets/Scripts/EnemyMovement.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyMovement : MonoBehaviour
+{
+    private NavMeshAgent agent;
+    public Transform player;
+
+    void Start(){
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    void Update(){
+        agent.SetDestination(player.position);
+    }
+}
