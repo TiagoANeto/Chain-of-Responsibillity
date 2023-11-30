4e6bfff953c44dd5ba9fccbbf921cb7578f5e2cb
unknown <estevaowerneck@gmail.com>
2023-11-29 21:19:05 -0300

    Atualizando movimentacao

--- PDJ_COR/Assets/Scripts/temp_PMovement.cs
+++ PDJ_COR/Assets/Scripts/temp_PMovement.cs
@@ -14,6 +14,7 @@ public class temp_PMovement : MonoBehaviour
     private Vector3 rotation;
     public Transform player;
     public float rotationSpeed = 5.0f; 
+    public float moveRotationSpeed = 5.0f;
     public Ray ray;
     public RaycastHit hit; 
     public bool freezeY = true;
@@ -37,55 +38,65 @@ public class temp_PMovement : MonoBehaviour
 
     
     void Update()
+{
+    // Verifica se o jogador está no chão
+    if (characterController.isGrounded)
     {
-        // Verifica se o jogador está no chão
-        if (characterController.isGrounded)
-        {
-            // Obtém a direção do movimento
-            Vector3 moveDirection = new Vector3(movement.x, 0, movement.y);
+        // Obtém a direção do movimento
+        Vector3 moveDirection = new Vector3(movement.x, 0, movement.y);
 
-            // Aplica o movimento à posição do jogador
-            characterController.Move(moveDirection * speed * Time.deltaTime);
+        // Se houver movimento, rotaciona o jogador suavemente na direção do movimento
+        if (moveDirection.magnitude > 0.1f)
+        {
+            Quaternion targetRotation = Quaternion.LookRotation(moveDirection.normalized);
+            player.rotation = Quaternion.Slerp(player.rotation, targetRotation, moveRotationSpeed * Time.deltaTime);
+        }
+
+        // Aplica o movimento à posição do jogador
+        characterController.Move(moveDirection * speed * Time.deltaTime);
+
+        anim.SetFloat("walkIdle", moveDirection.magnitude);
+    }
+
+    verticalVelocity -= gravity * Time.deltaTime;
+
+    characterController.Move(new Vector3(0, verticalVelocity, 0) * Time.deltaTime);
+
+
+    ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+    if (!Physics.Raycast(ray, out hit))
+    {
+        return;
+    }
+
+    Vector3 pos = hit.point - player.position;
+
+    if (freezeY)
+    {
+        pos.y = 0;
+    }
+
+    rot = Quaternion.LookRotation(pos);
+}
+
+    public void Attack(InputAction.CallbackContext context)
+    {
+        if(context.started) //Ocorre apenas no frame em que foi pressionado
+        {
+            player.rotation = Quaternion.Lerp(player.rotation, rot, rotationSpeed * Time.deltaTime);
             
+            Collider[] hitEnemies = Physics.OverlapSphere(attackPoint.position, attackRange, enemyLayers);
 
-            anim.SetFloat("walkIdle", moveDirection.magnitude);
+            foreach(Collider enemy in hitEnemies)
+            {
+                Debug.Log("We hit " + enemy.name);
+            }
         }
-
-        verticalVelocity -= gravity * Time.deltaTime;
-       
-        characterController.Move(new Vector3(0, verticalVelocity, 0) * Time.deltaTime);
-
-        ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-
-        if(!Physics.Raycast(ray, out hit)){
-            return;
-        }
-
-
-        Vector3 pos = hit.point - player.position;
-
-        if(freezeY){
-            pos.y = 0;
-        }
-
-        rot = Quaternion.LookRotation(pos);
-
-        
     }
 
-    public void Attack(InputAction.CallbackContext button)
+    void OnDrawGizmosSelected()
     {
-        player.rotation = Quaternion.Lerp(player.rotation, rot, rotationSpeed * Time.deltaTime);
-        
-        Collider[] hitEnemies = Physics.OverlapSphere(attackPoint.position, attackRange, enemyLayers);
-
-        foreach(Collider enemy in hitEnemies)
-        {
-            Debug.Log("We hit " + enemy.name);
-        }
-    }
-
-    void OnDrawGizmosSelected(){
         if(attackPoint==null){
             return;
         }
